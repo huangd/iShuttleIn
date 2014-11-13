c8d9e794c7cbe0e819dd2cb157d6cac05561e4ed
Di Huang <dhuang@linkedin.com>
2014-11-12 16:50:02 -0800

    Fix the CLLocationManager issue for IOS8

--- iShuttleIn/SIHomeViewController.m
+++ iShuttleIn/SIHomeViewController.m
@@ -47,214 +47,217 @@
 @implementation SIHomeViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        UIBarButtonItem *burger = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"burger.png"]
-                                                                   style:UIBarButtonItemStylePlain
-                                                                  target:self action:@selector(tapBurger:)];
-        self.navigationItem.leftBarButtonItem = burger;
-    }
-    return self;
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    UIBarButtonItem *burger = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"burger.png"]
+                                                               style:UIBarButtonItemStylePlain
+                                                              target:self action:@selector(tapBurger:)];
+    self.navigationItem.leftBarButtonItem = burger;
+  }
+  return self;
 }
 
 #pragma mark
 #pragma mark ViewController
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Setup CLLocationManager
-    self.locationManager = [[CLLocationManager alloc] init];
-    self.locationManager.delegate = self;
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    //    self.locationManager.distanceFilter = 10;
-    [self.locationManager startUpdatingLocation];
-    self.shuttleInAPIClient = [[SIShuttleInAPIClient alloc] init];
-    //Get ETA
-    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(shuttleETA) userInfo:nil repeats:YES];
-    
-    [self setupCounterLabel];
-    [self setupTimeLabel];
+  [super viewDidLoad];
+  // Setup CLLocationManager
+  self.locationManager = [[CLLocationManager alloc] init];
+  self.locationManager.delegate = self;
+  self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  // Check for iOS 8. Without this guard the code will crash with "unknown selector" on iOS 7.
+  if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+    [self.locationManager requestWhenInUseAuthorization];
+  }
+  [self.locationManager startUpdatingLocation];
+  self.shuttleInAPIClient = [[SIShuttleInAPIClient alloc] init];
+  //Get ETA
+  [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(shuttleETA) userInfo:nil repeats:YES];
+  
+  [self setupCounterLabel];
+  [self setupTimeLabel];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    [self setupCircle];
-    [self setupStatusLine];
+  [super viewWillAppear:animated];
+  [self setupCircle];
+  [self setupStatusLine];
 }
 
 - (IBAction)tapBurger:(UIBarButtonItem *)sender {
-    NSArray *images = @[
-                        [UIImage imageNamed:@"bus"],
-                        [UIImage imageNamed:@"marker"],
-                        ];
-    
-    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images];
-    callout.delegate = self;
-    [callout show];
+  NSArray *images = @[
+                      [UIImage imageNamed:@"bus"],
+                      [UIImage imageNamed:@"marker"],
+                      ];
+  
+  RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images];
+  callout.delegate = self;
+  [callout show];
 }
 
 #pragma mark
 #pragma mark iShuttle
 - (void)updateDirectionFrom:(SIGeoLocation *)from
                          to:(SIGeoLocation *)to {
-    [self.shuttleInAPIClient directionFrom:from
-                                        to:to
-                                  callback: ^(NSError *error, SIDirection *direction) {
-                                      if (error == nil) {
-                                          self.youDirection = direction;
-                                          [self.timeLabel setStartValue:direction.time*1000];
-                                          self.distanceLabel.text = [NSString stringWithFormat:@"%.1f", direction.distance];
-                                      }
-                                  }];
+  [self.shuttleInAPIClient directionFrom:from
+                                      to:to
+                                callback: ^(NSError *error, SIDirection *direction) {
+                                  if (error == nil) {
+                                    self.youDirection = direction;
+                                    [self.timeLabel setStartValue:direction.time*1000];
+                                    self.distanceLabel.text = [NSString stringWithFormat:@"%.1f", direction.distance];
+                                  }
+                                }];
 }
 
 - (void)shuttleETA {
-    self.shuttleStop = [[SIGeoLocation alloc] initWithLat:[self.stopTableViewController.stop objectForKey:@"Latitude"]
-                                                      lng:[self.stopTableViewController.stop objectForKey:@"Longitude"]];
-    self.navigationItem.title = [self.routeTableViewController.route objectForKey:@"ShortName"];
-    self.stopLabel.text = self.stopTableViewController.stopName;
-    
-    [self.shuttleInAPIClient shuttleETA:self.routeTableViewController.vehicleId
-                                     to:self.shuttleStop
-                               callback:^(NSError *error, SIDirection *direction) {
-                                   if (error == nil) {
-                                       self.shuttleDirection = direction;
-                                       [self.shuttleTimeLabel setStartValue:direction.time*1000];
-                                       self.shuttleDistanceLabel.text = [NSString stringWithFormat:@"%.1f", direction.distance];
-                                       //Update counterLabel
-                                       int timeDiff = self.shuttleDirection.time - self.youDirection.time;
-                                       if (timeDiff > 0) {
-                                           [self.counterLabel stop];
-                                           self.counterLabel.startValue = timeDiff*1000;
-                                           [self.counterLabel start];
-                                       }
-                                   }
-                               }];
+  self.shuttleStop = [[SIGeoLocation alloc] initWithLat:[self.stopTableViewController.stop objectForKey:@"Latitude"]
+                                                    lng:[self.stopTableViewController.stop objectForKey:@"Longitude"]];
+  self.navigationItem.title = [self.routeTableViewController.route objectForKey:@"ShortName"];
+  self.stopLabel.text = self.stopTableViewController.stopName;
+  
+  [self.shuttleInAPIClient shuttleETA:self.routeTableViewController.vehicleId
+                                   to:self.shuttleStop
+                             callback:^(NSError *error, SIDirection *direction) {
+                               if (error == nil) {
+                                 self.shuttleDirection = direction;
+                                 [self.shuttleTimeLabel setStartValue:direction.time*1000];
+                                 self.shuttleDistanceLabel.text = [NSString stringWithFormat:@"%.1f", direction.distance];
+                                 //Update counterLabel
+                                 int timeDiff = self.shuttleDirection.time - self.youDirection.time;
+                                 if (timeDiff > 0) {
+                                   [self.counterLabel stop];
+                                   self.counterLabel.startValue = timeDiff*1000;
+                                   [self.counterLabel start];
+                                 }
+                               }
+                             }];
 }
 
 #pragma mark
 #pragma RNFrostedSidebarDelegate
 -(void)sidebar:(RNFrostedSidebar *)sidebar didTapItemAtIndex:(NSUInteger)index {
-    switch (index) {
-        case 0: {
-            [sidebar dismissAnimated:YES completion:^(BOOL finished) {
-                if (finished) {
-                    if (self.routeTableViewController == nil) {
-                        self.routeTableViewController = [[SIRouteTableViewController alloc] init];
-                    }
-                    [self.navigationController pushViewController:self.routeTableViewController animated:YES];
-                }
-            }];
-            break;
+  switch (index) {
+    case 0: {
+      [sidebar dismissAnimated:YES completion:^(BOOL finished) {
+        if (finished) {
+          if (self.routeTableViewController == nil) {
+            self.routeTableViewController = [[SIRouteTableViewController alloc] init];
+          }
+          [self.navigationController pushViewController:self.routeTableViewController animated:YES];
         }
-        case 1: {
-            [sidebar dismissAnimated:YES completion:^(BOOL finished) {
-                if (finished) {
-                    if (self.stopTableViewController == nil) {
-                        self.stopTableViewController = [[SIStopTableViewController alloc] init];
-                    }
-                    self.stopTableViewController.routeId = self.routeTableViewController.routeId;
-                    [self.navigationController pushViewController:self.stopTableViewController animated:YES];
-                }
-            }];
-            break;
-        }
-        default: {
-            [sidebar dismissAnimated:YES];
-            break;
-        }
+      }];
+      break;
     }
+    case 1: {
+      [sidebar dismissAnimated:YES completion:^(BOOL finished) {
+        if (finished) {
+          if (self.stopTableViewController == nil) {
+            self.stopTableViewController = [[SIStopTableViewController alloc] init];
+          }
+          self.stopTableViewController.routeId = self.routeTableViewController.routeId;
+          [self.navigationController pushViewController:self.stopTableViewController animated:YES];
+        }
+      }];
+      break;
+    }
+    default: {
+      [sidebar dismissAnimated:YES];
+      break;
+    }
+  }
 }
 
 #pragma mark
 #pragma mark CLLocationManagerDelegate
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-    NSLog(@"Error: %@", error);
-    NSLog(@"Failed to get current location");
+  NSLog(@"Error: %@", error);
+  NSLog(@"Failed to get current location");
 }
 
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    SIGeoLocation *currentLocation = [[SIGeoLocation alloc] initWithLat:@([[locations lastObject] coordinate].latitude)
-                                                                    lng:@([[locations lastObject] coordinate].longitude)];
-    self.shuttleStop = [[SIGeoLocation alloc] initWithLat:[self.stopTableViewController.stop objectForKey:@"Latitude"]
-                                                      lng:[self.stopTableViewController.stop objectForKey:@"Longitude"]];
-    self.navigationItem.title = [self.routeTableViewController.route objectForKey:@"ShortName"];
-    self.stopLabel.text = self.stopTableViewController.stopName;
-    [self updateDirectionFrom:currentLocation to:self.shuttleStop];
+  SIGeoLocation *currentLocation = [[SIGeoLocation alloc] initWithLat:@([[locations lastObject] coordinate].latitude)
+                                                                  lng:@([[locations lastObject] coordinate].longitude)];
+  self.shuttleStop = [[SIGeoLocation alloc] initWithLat:[self.stopTableViewController.stop objectForKey:@"Latitude"]
+                                                    lng:[self.stopTableViewController.stop objectForKey:@"Longitude"]];
+  self.navigationItem.title = [self.routeTableViewController.route objectForKey:@"ShortName"];
+  self.stopLabel.text = self.stopTableViewController.stopName;
+  [self updateDirectionFrom:currentLocation to:self.shuttleStop];
 }
 
 #pragma mark
 #pragma mark TTCounterLabel
 - (void)setupCounterLabel {
-    CGFloat numberFont = 55;
-    CGFloat letterFont = 20;
-    [self.counterLabel setBoldFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:numberFont]];
-    [self.counterLabel setRegularFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:numberFont]];
-    // The font property of the label is used as the font for H,M,S and MS
-    [self.counterLabel setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:letterFont]];
-    // Default label properties
-    self.counterLabel.textColor = [UIColor darkGrayColor];
-    // After making any changes we need to call update appearance
-    [self.counterLabel updateApperance];
-    
-    self.counterLabel.countDirection = kCountDirectionDown;
-    self.counterLabel.displayMode = kDisplayModeSeconds;
+  CGFloat numberFont = 55;
+  CGFloat letterFont = 20;
+  [self.counterLabel setBoldFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:numberFont]];
+  [self.counterLabel setRegularFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:numberFont]];
+  // The font property of the label is used as the font for H,M,S and MS
+  [self.counterLabel setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:letterFont]];
+  // Default label properties
+  self.counterLabel.textColor = [UIColor darkGrayColor];
+  // After making any changes we need to call update appearance
+  [self.counterLabel updateApperance];
+  
+  self.counterLabel.countDirection = kCountDirectionDown;
+  self.counterLabel.displayMode = kDisplayModeSeconds;
 }
 
 - (void)setupTimeLabel {
-    CGFloat numberFont = 20;
-    CGFloat letterFont = 12;
-    [self.timeLabel setBoldFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:numberFont]];
-    [self.timeLabel setRegularFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:numberFont]];
-    // The font property of the label is used as the font for H,M,S and MS
-    [self.timeLabel setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:letterFont]];
-    // Default label properties
-    self.timeLabel.textColor = [UIColor darkGrayColor];
-    // After making any changes we need to call update appearance
-    [self.timeLabel updateApperance];
-    self.timeLabel.countDirection = kCountDirectionDown;
-    self.timeLabel.displayMode = kDisplayModeSeconds;
-    
-    [self.shuttleTimeLabel setBoldFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:numberFont]];
-    [self.shuttleTimeLabel setRegularFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:numberFont]];
-    // The font property of the label is used as the font for H,M,S and MS
-    [self.shuttleTimeLabel setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:letterFont]];
-    // Default label properties
-    self.shuttleTimeLabel.textColor = [UIColor darkGrayColor];
-    // After making any changes we need to call update appearance
-    [self.shuttleTimeLabel updateApperance];
-    self.shuttleTimeLabel.countDirection = kCountDirectionDown;
-    self.shuttleTimeLabel.displayMode = kDisplayModeSeconds;
-    
+  CGFloat numberFont = 20;
+  CGFloat letterFont = 12;
+  [self.timeLabel setBoldFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:numberFont]];
+  [self.timeLabel setRegularFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:numberFont]];
+  // The font property of the label is used as the font for H,M,S and MS
+  [self.timeLabel setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:letterFont]];
+  // Default label properties
+  self.timeLabel.textColor = [UIColor darkGrayColor];
+  // After making any changes we need to call update appearance
+  [self.timeLabel updateApperance];
+  self.timeLabel.countDirection = kCountDirectionDown;
+  self.timeLabel.displayMode = kDisplayModeSeconds;
+  
+  [self.shuttleTimeLabel setBoldFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:numberFont]];
+  [self.shuttleTimeLabel setRegularFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:numberFont]];
+  // The font property of the label is used as the font for H,M,S and MS
+  [self.shuttleTimeLabel setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:letterFont]];
+  // Default label properties
+  self.shuttleTimeLabel.textColor = [UIColor darkGrayColor];
+  // After making any changes we need to call update appearance
+  [self.shuttleTimeLabel updateApperance];
+  self.shuttleTimeLabel.countDirection = kCountDirectionDown;
+  self.shuttleTimeLabel.displayMode = kDisplayModeSeconds;
+  
 }
 
 
 #pragma mark
 #pragma mark SICircle
 - (void)setupCircle {
-    float radius = 110;
-    CGRect counterLabelFrame = self.counterLabel.frame;
-    CGPoint counterLabelCenterPoint = CGPointMake(counterLabelFrame.size.width/2+counterLabelFrame.origin.x,
-                                                  counterLabelFrame.size.height/2+counterLabelFrame.origin.y);
-    self.circle = [[SICircle alloc] initWithPosition:CGPointMake(counterLabelCenterPoint.x-radius,
-                                                                 counterLabelCenterPoint.y-radius+5)
-                                              radius:radius
-                                      internalRadius:radius-5
-                                   circleStrokeColor:[UIColor lightGrayColor]];
-    [self.view addSubview:self.circle];
-    
+  float radius = 110;
+  CGRect counterLabelFrame = self.counterLabel.frame;
+  CGPoint counterLabelCenterPoint = CGPointMake(counterLabelFrame.size.width/2+counterLabelFrame.origin.x,
+                                                counterLabelFrame.size.height/2+counterLabelFrame.origin.y);
+  self.circle = [[SICircle alloc] initWithPosition:CGPointMake(counterLabelCenterPoint.x-radius,
+                                                               counterLabelCenterPoint.y-radius+5)
+                                            radius:radius
+                                    internalRadius:radius-5
+                                 circleStrokeColor:[UIColor lightGrayColor]];
+  [self.view addSubview:self.circle];
+  
 }
 
 #pragma mark
 #pragma mark SIStatusLine
 - (void)setupStatusLine {
-    CGFloat lineLength = 300;
-    CGFloat lineWidth = 8;
-    self.statusLine = [[SIStatusLine alloc] initWithPosition:CGPointMake(self.view.frame.size.width/2 - lineLength/2,
-                                                                         self.view.frame.size.height - 100)
-                                                   lineWidth:lineWidth
-                                                  lineLength:lineLength
-                                                    iconSize:25
-                                             lineStrokeColor:[UIColor lightGrayColor]];
-    [self.view addSubview:self.statusLine];
+  CGFloat lineLength = 300;
+  CGFloat lineWidth = 8;
+  self.statusLine = [[SIStatusLine alloc] initWithPosition:CGPointMake(self.view.frame.size.width/2 - lineLength/2,
+                                                                       self.view.frame.size.height - 100)
+                                                 lineWidth:lineWidth
+                                                lineLength:lineLength
+                                                  iconSize:25
+                                           lineStrokeColor:[UIColor lightGrayColor]];
+  [self.view addSubview:self.statusLine];
 }
 @end
